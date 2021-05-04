372e4d68243e89ac9d9b9ace61c657d72770f568
Michael Cusack <80846125+mpcusack-color@users.noreply.github.com>
2021-05-04 16:49:26 -0700

    Remove unused cruft from clr (#13)

--- clr/config.py
+++ clr/config.py
@@ -1,7 +1,13 @@
-from past.builtins import execfile
+"""The clr tool is configred using a python source file (clrfile.py) that is
+distributed externally to this tool. On start up we scan for this file and
+import it to initialize the command list
+"""
+
 from pathlib import Path
 import os.path
 import os
+import runpy
+
 
 def find_clrfile(name='clrfile.py'):
     """Scan for `clrfile.py` defining clr command namespaces.
@@ -23,16 +29,12 @@ def find_clrfile(name='clrfile.py'):
             return file_path
     raise Exception("%s could not be located. Searched in %s" % (name, search_paths))
 
-def commands():
-    config = _get_config()
-    return config['commands']
+def read_namespaces():
+    """Returns a mapping from namespace keys to python module paths.
 
-def options():
-    config = _get_config()
-    return config['options']
-
-_config_cache = {}
-def _get_config():
-    if not _config_cache:
-        execfile(find_clrfile(), _config_cache)
-    return _config_cache
+    find_clrfile() returns a filesystem path. It may not be in the PYTHONPATH.
+    Use runpy to "import" the clrfile into a isolated namespace and extract the
+    'commands'. The values of this mapping are python module names that *are* on
+    the PYTHONPATH and can be imported with importlib.import_module.
+    """
+    return runpy.run_path(find_clrfile())['commands']
