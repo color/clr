ce6f4bc12b1dceb906bac6fd96f32e808b2f99bc
Michael Cusack <80846125+mpcusack-color@users.noreply.github.com>
2021-05-18 09:25:42 -0700

    Bump setup.py version to 0.3.0.
    A github release was previously made for 0.2.0 w/o updating this file. This will bring them into sync.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = ["dataclasses==0.8;python_version<'3.7'"]
 
 setup(
     name="clr",
-    version="0.2.0",
+    version="0.3.0",
     description="A command line tool for executing custom python scripts.",
     author="Color",
     author_email="dev@getcolor.com",
