5dcf9c5f766e933b592f1c1682c4f420cf9da755
Michael Cusack <80846125+mpcusack-color@users.noreply.github.com>
2021-08-16 14:53:19 -0700

    Better error message for enum parsing failure (#30)

--- clr/commands.py
+++ clr/commands.py
@@ -149,7 +149,7 @@ def _get_arg_type(param):
             arg = arg.upper()
             assert (
                 arg in param.annotation.__members__
-            ), f"Arg ({param.name}) must be one of {[v.name for v in param.annotation]}."
+            ), f"Arg ({param.name}={arg}) must be one of {tuple(annotation.__members__.keys())}."
             return param.annotation.__members__[arg]
 
         return enum_parser
