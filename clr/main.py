a23fc0c53c534b9780652d6df2bb8e2e3f166342
Jon Carl <jon.carl@color.com>
2021-10-07 10:56:04 -0600

    feat: add honeycomb for analytics
    Signed-off-by: Jon Carl <jon.carl@color.com>

--- clr/main.py
+++ clr/main.py
@@ -1,6 +1,22 @@
 import sys
+import os
+import getpass
+import socket
+import beeline
 from clr.commands import resolve_command, get_namespace
+from clrenv import env
 
+def init_beeline(service_name):
+    if env.honeycomb.writekey is not None:
+        try:
+            beeline.init(
+                writekey=env.honeycomb.writekey,
+                dataset='clr',
+                service_name=service_name,
+                debug=False,
+            )
+        except Exception as e:
+            print('Failed to initialize beeline: %s', e)
 
 def main(argv=None):
     if not argv:
@@ -13,13 +29,36 @@ def main(argv=None):
     namespace = get_namespace(namespace_key)
     bound_args = namespace.parse_args(cmd_name, argv[2:])
 
-    # Call the command.
+    init_beeline(namespace_key)
+    trace = beeline.start_trace(context={
+        "name": cmd_name,
+        "username": getpass.getuser(),
+    })
+
     if hasattr(namespace.instance, "cmdinit"):
+        cmdinit_span = beeline.start_span({"name": "cmdinit"})
         namespace.instance.cmdinit()
-    result = namespace.command_callables[cmd_name](
-        *bound_args.args, **bound_args.kwargs
-    )
+        beeline.finish_span(cmdinit_span)
+
+    cmdrun_span = beeline.start_span({"name": "cmdrun"})
+    exit_code = 0
+    result = None
+    try:
+        result = namespace.command_callables[cmd_name](
+            *bound_args.args, **bound_args.kwargs
+        )
+    except Exception as e:
+        print('Error running command', e)
+        beeline.add_context_field('raised_exception', True)
+        exit_code = 1
+
+    beeline.finish_span(cmdrun_span)
+
     # Support exit codes. This supports int and bool return values.
-    if isinstance(result, (int, bool)):
-        # print(f"Result: {result}", file=sys.stderr)
-        sys.exit(result)
+    if result is not None and isinstance(result, (int, bool)):
+        exit_code = result
+
+    beeline.add_context_field('exit_code', exit_code)
+    beeline.finish_trace(trace)
+    beeline.close()
+    sys.exit(exit_code)
