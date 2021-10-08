ede9849a0df7a42697d9458b1011d39358902b11
Michael Cusack <michael.cusack@color.com>
2021-10-08 16:00:57 -0700

    restructure

--- clr/main.py
+++ clr/main.py
@@ -3,11 +3,14 @@ import os
 import getpass
 import beeline
 import socket
+import traceback
+from contextlib import contextmanager
 from clr.commands import resolve_command, get_namespace
 from clrenv import env
 
 
-def init_beeline(service_name):
+@contextmanager
+def init_beeline(service_name, cmd_name):
     # clrenv < 0.2.0 has a bug in the `in` operator at the root level.
     if env.get('honeycomb') is not None:
         try:
@@ -18,7 +21,20 @@ def init_beeline(service_name):
                 debug=False,
             )
         except Exception as e:
+            # Honeycomb logging is completely optional and all later calls to
+            # beeline are silently no-ops if not initialized. Simply log the
+            # failure and continue normally.
             print('Failed to initialize beeline: %s', e, file=sys.stderr)
+
+        yield beeline.start_trace(
+            context={
+                "name": cmd_name,
+                "username": getpass.getuser(),
+                "hostname": socket.gethostname(),
+            }
+        )
+        beeline.finish_trace(trace)
+        beeline.close()
 
 
 def main(argv=None):
@@ -32,39 +48,28 @@ def main(argv=None):
     namespace = get_namespace(namespace_key)
     bound_args = namespace.parse_args(cmd_name, argv[2:])
 
-    init_beeline(namespace_key)
-    trace = beeline.start_trace(
-        context={
-            "name": cmd_name,
-            "username": getpass.getuser(),
-            "hostname": socket.gethostname(),
-        }
-    )
-
-    if hasattr(namespace.instance, "cmdinit"):
-        with beeline.tracer(name="cmdinit"):
-            namespace.instance.cmdinit()
-
-    cmdrun_span = beeline.start_span({"name": "cmdrun"})
     exit_code = 0
-    result = None
-    try:
-        result = namespace.command_callables[cmd_name](
-            *bound_args.args, **bound_args.kwargs
-        )
-    except Exception as e:
-        print('Error running command: %s', e, file=sys.stderr)
-        beeline.add_context_field('raised_exception', True)
-        exit_code = 1
 
-    beeline.finish_span(cmdrun_span)
+    with init_beeline(namespace_key, cmd_name):
+        # Some namespaces define a cmdinit function which should be run first.
+        if hasattr(namespace.instance, "cmdinit"):
+            with beeline.tracer(name="cmdinit"):
+                namespace.instance.cmdinit()
 
-    # Support exit codes. This supports int and bool return values.
-    if result is not None and isinstance(result, (int, bool)):
-        exit_code = result
+        with beeline.tracer("cmdrun"):
+            result = None
+            try:
+                result = namespace.command_callables[cmd_name](
+                    *bound_args.args, **bound_args.kwargs
+                )
+                # Support exit codes. This supports int and bool return values.
+                if isinstance(result, (int, bool)):
+                    exit_code = result
+            except:
+                print(traceback.format_exc(), file=sys.stderr)
+                beeline.add_context_field('raised_exception', True)
+                exit_code = 999
 
-    beeline.add_context_field('exit_code', exit_code)
-    beeline.finish_trace(trace)
-    beeline.close()
+            beeline.add_context_field('exit_code', exit_code)
 
     sys.exit(exit_code)
