53f95ee79391198a7e711fa1fa797333d68a0ad5
Jon Carl <jon.carl@color.com>
2021-10-13 12:24:58 -0600

    feat: add honeycomb for analytics (#33)

--- clr/main.py
+++ clr/main.py
@@ -1,5 +1,54 @@
 import sys
+import os
+import getpass
+import beeline
+import platform
+import traceback
+import signal
+from contextlib import contextmanager
 from clr.commands import resolve_command, get_namespace
+
+DEBUG_MODE = os.environ.get("CLR_DEBUG", "").lower() in ("true", "1")
+
+def on_exit(signum, frame):
+    beeline.add_trace_field("killed_by_signal", signal.Signals(signum).name)
+    beeline.close()
+
+signal.signal(signal.SIGINT, on_exit)
+signal.signal(signal.SIGTERM, on_exit)
+
+@contextmanager
+def init_beeline(namespace_key, cmd_name):
+    try:
+        from clrenv import env
+
+        # clrenv < 0.2.0 has a bug in the `in` operator at the root level.
+        if env.get("honeycomb") is not None:
+            beeline.init(
+                writekey=env.honeycomb.writekey,
+                dataset="clr",
+                service_name="clr",
+                debug=False,
+            )
+    except:
+        # Honeycomb logging is completely optional and all later calls to
+        # beeline are silently no-ops if not initialized. Simply log the
+        # failure and continue normally. This includes if clrenv can not be
+        # loaded.
+        if DEBUG_MODE:
+            print("Failed to initialize beeline.", file=sys.stderr)
+            traceback.print_exc()
+
+    with beeline.tracer("cmd"):
+        beeline.add_trace_field("namespace", namespace_key)
+        beeline.add_trace_field("cmd", cmd_name)
+        beeline.add_trace_field("username", getpass.getuser())
+        beeline.add_trace_field("hostname", platform.node())
+
+        # Bounce back to the calling code.
+        yield
+
+    beeline.close()
 
 
 def main(argv=None):
@@ -11,15 +60,32 @@ def main(argv=None):
 
     namespace_key, cmd_name = resolve_command(query)
     namespace = get_namespace(namespace_key)
-    bound_args = namespace.parse_args(cmd_name, argv[2:])
 
-    # Call the command.
-    if hasattr(namespace.instance, "cmdinit"):
-        namespace.instance.cmdinit()
-    result = namespace.command_callables[cmd_name](
-        *bound_args.args, **bound_args.kwargs
-    )
-    # Support exit codes. This supports int and bool return values.
-    if isinstance(result, (int, bool)):
-        # print(f"Result: {result}", file=sys.stderr)
-        sys.exit(result)
+    # Default successful exit code.
+    exit_code = 0
+
+    with init_beeline(namespace_key, cmd_name):
+        with beeline.tracer("parse_args"):
+            bound_args = namespace.parse_args(cmd_name, argv[2:])
+
+        # Some namespaces define a cmdinit function which should be run first.
+        if hasattr(namespace.instance, "cmdinit"):
+            with beeline.tracer("cmdinit"):
+                namespace.instance.cmdinit()
+
+        with beeline.tracer("cmdrun"):
+            result = None
+            try:
+                result = namespace.command_callables[cmd_name](
+                    *bound_args.args, **bound_args.kwargs
+                )
+                if isinstance(result, (int, bool)):
+                    exit_code = int(result)
+            except:
+                print(traceback.format_exc(), file=sys.stderr)
+                beeline.add_trace_field("raised_exception", True)
+                exit_code = 999
+
+        beeline.add_trace_field("exit_code", exit_code)
+
+    sys.exit(exit_code)
