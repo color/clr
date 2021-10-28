11262ff8e2ae99c95c5d86f609ec5a948df26b08
Michael Cusack <80846125+mpcusack-color@users.noreply.github.com>
2021-10-28 09:27:51 -0700

    Make honeycomb logging an event rather than a span (#40)
    Previous version had two sets of issues:
    
    Use of signals for beeline cleanup stoped control-c and django hot reload from working.
    Using beeline traces interfered with instrumentation in commands.
    To fix these we move all clr honeycomb logging to a single event sent via an atexit function.
    This PR also moves the package version into its own file so we can report it to honeycomb. No great ways to do that in python 3.6 (see https://packaging.python.org/guides/single-sourcing-package-version/), but this seemed the cleanest.

--- clr/main.py
+++ clr/main.py
@@ -2,53 +2,66 @@ import sys
 import os
 import getpass
 import beeline
-import platform
+import time
 import traceback
-import signal
+import atexit
 from contextlib import contextmanager
-from clr.commands import resolve_command, get_namespace
+from .commands import resolve_command, get_namespace
+from ._version import __version__
 
 DEBUG_MODE = os.environ.get("CLR_DEBUG", "").lower() in ("true", "1")
 
-def on_exit(signum, frame):
-    beeline.add_trace_field("killed_by_signal", signal.Signals(signum).name)
-    beeline.close()
+# Store data to send to honeycomb as a global so it can be accessed from an
+# atexit method. None of this code should ever be called from within a long
+# running process.
+honeycomb_data = {}
 
-signal.signal(signal.SIGINT, on_exit)
-signal.signal(signal.SIGTERM, on_exit)
 
-@contextmanager
-def init_beeline(namespace_key, cmd_name):
+def send_to_honeycomb():
+    """Attempts to log usage data to honeycomb.
+
+    Honeycomb logging is completely optional. If there are any failures
+    simply continue as normal. This includes if clrenv can not be loaded.
+
+    These metrics are sent to honeycomb as an event rather than a trace for two
+    reasons:
+    1) They don't really fit the web requests model.
+    2) Downstream code might have its own tracing which we don't want to interfere
+    with.
+    """
     try:
         from clrenv import env
 
         # clrenv < 0.2.0 has a bug in the `in` operator at the root level.
-        if env.get("honeycomb") is not None:
-            beeline.init(
-                writekey=env.honeycomb.writekey,
-                dataset="clr",
-                service_name="clr",
-                debug=False,
-            )
+        if env.get("honeycomb") is None:
+            return
+
+        beeline.init(
+            writekey=env.honeycomb.writekey,
+            dataset="clr",
+            service_name="clr",
+        )
+
+        # Convert start_time into a duration.
+        honeycomb_data["duration_ms"] = int(
+            1000 * (time.time() - honeycomb_data["start_time"])
+        )
+        del honeycomb_data["start_time"]
+
+        honeycomb_data["username"] = getpass.getuser()
+        honeycomb_data["clr_version"] = __version__
+        honeycomb_data["color_key_mode"] = env.key_mode
+
+        beeline.send_now(honeycomb_data)
+        beeline.close()
     except:
-        # Honeycomb logging is completely optional and all later calls to
-        # beeline are silently no-ops if not initialized. Simply log the
-        # failure and continue normally. This includes if clrenv can not be
-        # loaded.
         if DEBUG_MODE:
             print("Failed to initialize beeline.", file=sys.stderr)
-            traceback.print_exc()
+            print(traceback.format_exc(), file=sys.stderr)
 
-    with beeline.tracer("cmd"):
-        beeline.add_trace_field("namespace", namespace_key)
-        beeline.add_trace_field("cmd", cmd_name)
-        beeline.add_trace_field("username", getpass.getuser())
-        beeline.add_trace_field("hostname", platform.node())
 
-        # Bounce back to the calling code.
-        yield
-
-    beeline.close()
+# Will get run on normal completion, exceptions and sys.exit.
+atexit.register(send_to_honeycomb)
 
 
 def main(argv=None):
@@ -59,33 +72,36 @@ def main(argv=None):
         query = argv[1]
 
     namespace_key, cmd_name = resolve_command(query)
+
+    honeycomb_data["namespace_key"] = namespace_key
+    honeycomb_data["cmd_name"] = cmd_name
+    honeycomb_data["start_time"] = time.time()
+
     namespace = get_namespace(namespace_key)
 
     # Default successful exit code.
     exit_code = 0
 
-    with init_beeline(namespace_key, cmd_name):
-        with beeline.tracer("parse_args"):
-            bound_args = namespace.parse_args(cmd_name, argv[2:])
+    bound_args = namespace.parse_args(cmd_name, argv[2:])
+    # TODO(michael.cusack): Args are currently not logged to honeycomb for PHI reasons.
+    # Evaluate whether this is really an issue.
+    # honeycomb_data['args'] = bound_args.arguments
 
-        # Some namespaces define a cmdinit function which should be run first.
-        if hasattr(namespace.instance, "cmdinit"):
-            with beeline.tracer("cmdinit"):
-                namespace.instance.cmdinit()
+    # Some namespaces define a cmdinit function which should be run first.
+    if hasattr(namespace.instance, "cmdinit"):
+        namespace.instance.cmdinit()
 
-        with beeline.tracer("cmdrun"):
-            result = None
-            try:
-                result = namespace.command_callables[cmd_name](
-                    *bound_args.args, **bound_args.kwargs
-                )
-                if isinstance(result, (int, bool)):
-                    exit_code = int(result)
-            except:
-                print(traceback.format_exc(), file=sys.stderr)
-                beeline.add_trace_field("raised_exception", True)
-                exit_code = 999
+    result = None
+    try:
+        result = namespace.command_callables[cmd_name](
+            *bound_args.args, **bound_args.kwargs
+        )
+        if isinstance(result, (int, bool)):
+            exit_code = int(result)
+    except BaseException as e:
+        # BaseException so we still will see KeyboardInterrupts
+        honeycomb_data["raised_exception"] = repr(e)
+        raise
 
-        beeline.add_trace_field("exit_code", exit_code)
-
+    honeycomb_data["exit_code"] = exit_code
     sys.exit(exit_code)
